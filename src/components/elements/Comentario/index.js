d3b1632688083ed117dabb24d2cc3f7a66ce83fc
DanielSalis <danielsalis98@gmail.com>
2022-01-12 21:57:06 -0300

    adding trash icon

--- src/components/elements/Comentario/index.js
+++ src/components/elements/Comentario/index.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
 
 import {Stack, Box, Text, Flex} from '@chakra-ui/layout';
 import {Avatar} from '@chakra-ui/avatar';
+import {FiTrash} from 'react-icons/fi';
+import {Context as AuthContext} from '../../stores/Auth';
 
 const Comentario = ({item} = {}) => {
+  const {user} = useContext(AuthContext);
+
+  const deleteComment = (commentId) => {
+    console.log(commentId);
+  };
+
+  debugger;
   return (
     <Flex flexDirection="row" align="flex-start">
-      <Box mr={{base: 2, lg: 4}}>
+      <Box mr={{base: 2, lg: 3}}>
         <Avatar name={item.author?.name} src={item.author?.avatar} />
       </Box>
-      <Box p={{base: 3, lg: 4}} background="#ddd" borderRadius="10px">
+      <Box p={{base: 3, lg: 3}} background="#ddd" borderRadius="10px">
         <Stack direction="row" justifyContent="space-between">
           <Text mb={4} fontWeight="bold" fontSize="xs" color="black">
             {item.author?.name}
+            <Text fontSize="xs" color="gray">
+              {item.dateTime.fromNow()}
+            </Text>
           </Text>
-          <Text fontSize="xs" color="gray">
-            {item.dateTime.fromNow()}
-          </Text>
+          {user.id === item.author.id ? (
+            <FiTrash size={15} onClick={() => deleteComment()} />
+          ) : (
+            <></>
+          )}
         </Stack>
 
         <Text fontSize="sm" color="black" align="justify">
