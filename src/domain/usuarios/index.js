9989f5c177244a8a522bdbf068d7bbbcfe9e056c
DanielSalis <danielsalis98@gmail.com>
2022-01-22 17:57:35 -0300

    add função de inativação no index

--- src/domain/usuarios/index.js
+++ src/domain/usuarios/index.js
@@ -3,4 +3,4 @@ export {default as create} from './create';
 export {default as updateById} from './updateById';
 export {default as verifyUsername} from './verifyUsername';
 export {default as getAll} from './getAll';
-export {default as deleteById} from './deleteById';
+export {default as inactivateById} from './inactivateById';
