8514e03fc9496ccbe75cdb48242ffc3790ee355a
DanielSalis <danielsalis98@gmail.com>
2022-01-22 19:53:09 -0300

    importando activateById

--- src/domain/usuarios/index.js
+++ src/domain/usuarios/index.js
@@ -4,3 +4,4 @@ export {default as updateById} from './updateById';
 export {default as verifyUsername} from './verifyUsername';
 export {default as getAll} from './getAll';
 export {default as inactivateById} from './inactivateById';
+export {default as activateById} from './activateById';
