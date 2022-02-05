3730c5ee8afdece9a6e9c00646a6ab5fe6281f4b
DanielSalis <danielsalis98@gmail.com>
2022-02-05 12:18:22 -0300

    add delete by id nas funções globais

--- src/domain/comentarios/delete.js
+++ src/domain/comentarios/delete.js
@@ -2,20 +2,23 @@ import {isEmpty, isNil, isString} from 'lodash';
 import api from '../../services/api';
 
 // eslint-disable-next-line func-names
-export default async function (token, id) {
+export default async function (token, commentId) {
   if (isNil(token) || isEmpty(token))
     throw new Error('Token não foi informado');
 
-  if (isNil(id) || (isEmpty(id) && isString(id)))
+  if (isNil(commentId) || (isEmpty(commentId) && isString(commentId)))
     throw new Error('Id do comentário não foi informado');
 
-  const response = await api.delete(`/commentarios/${id}`, {
+  const requestData = {
+    comentario_id: commentId,
+  };
+
+  const response = await api.delete(`/comentarios`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
+    data: requestData,
   });
 
-  console.log(response);
-
-  return {};
+  return response;
 }
