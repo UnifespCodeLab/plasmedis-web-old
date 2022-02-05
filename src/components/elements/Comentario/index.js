8f0416d869dbde6d6b3f2bfbd84f6dd7c3dba196
DanielSalis <danielsalis98@gmail.com>
2022-02-05 12:18:38 -0300

    integrando com o backend

--- src/components/elements/Comentario/index.js
+++ src/components/elements/Comentario/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import PropTypes from 'prop-types';
 import {Anchorme} from 'react-anchorme';
 import {Stack, Box, Text, Flex} from '@chakra-ui/layout';
@@ -6,10 +6,12 @@ import {Avatar} from '@chakra-ui/avatar';
 import {FiTrash} from 'react-icons/fi';
 import {Context as AuthContext} from '../../stores/Auth';
 import {TextAnchor} from './styles.js';
+import {deleteById} from '../../../domain/comentarios';
 
 const Comentario = ({item} = {}) => {
   debugger;
-  const {user} = useContext(AuthContext);
+  const {user, token} = useContext(AuthContext);
+  const [hideComponent, setHideComponent] = useState(false);
 
   const checkIfIsAbleToDelete = () => {
     if (
@@ -23,36 +25,46 @@ const Comentario = ({item} = {}) => {
     return false;
   };
 
-  const deleteComment = (commentId) => {
-    console.log('Teste', commentId);
+  const showDeleteDialog = async (commentId) => {
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm('Deseja realmente excluir esse comentário?')) {
+      const response = await deleteById(token, commentId);
+      if (response.status === 200) {
+        setHideComponent(true);
+      }
+    }
   };
 
   return (
     <Flex flexDirection="row" align="flex-start">
-      <Box mr={{base: 2, lg: 3}}>
-        <Avatar name={item.author?.name} src={item.author?.avatar} />
-      </Box>
-      <Box p={{base: 3, lg: 3}} background="#ddd" borderRadius="10px">
-        <Stack direction="row" justifyContent="space-between">
-          <Text mb={4} fontWeight="bold" fontSize="xs" color="black">
-            {item.author?.name}
-            <Text fontSize="xs" color="gray">
-              {item.dateTime.fromNow()}
-            </Text>
-          </Text>
-          {checkIfIsAbleToDelete() ? (
-            <FiTrash size={15} onClick={() => deleteComment(item.id)} />
-          ) : (
-            <></>
-          )}
-        </Stack>
+      {!hideComponent && (
+        <>
+          <Box mr={{base: 2, lg: 3}}>
+            <Avatar name={item.author?.name} src={item.author?.avatar} />
+          </Box>
+          <Box p={{base: 3, lg: 3}} background="#ddd" borderRadius="10px">
+            <Stack direction="row" justifyContent="space-between">
+              <Text mb={4} fontWeight="bold" fontSize="xs" color="black">
+                {item.author?.name}
+                <Text fontSize="xs" color="gray">
+                  {item.dateTime.fromNow()}
+                </Text>
+              </Text>
+              {checkIfIsAbleToDelete() ? (
+                <FiTrash size={15} onClick={() => showDeleteDialog(item.id)} />
+              ) : (
+                <></>
+              )}
+            </Stack>
 
-        <TextAnchor size="sm" color="black" align="justify">
-          <Anchorme target="_blank" rel="noreferrer noopener">
-            {item.body}
-          </Anchorme>
-        </TextAnchor>
-      </Box>
+            <TextAnchor size="sm" color="black" align="justify">
+              <Anchorme target="_blank" rel="noreferrer noopener">
+                {item.body}
+              </Anchorme>
+            </TextAnchor>
+          </Box>
+        </>
+      )}
     </Flex>
   );
 };
